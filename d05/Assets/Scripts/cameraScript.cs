de4016c7f072330999d85301ad3a7e553abcc4b7
Your Name <you@example.com>
2019-04-08 16:33:30 +0200

    save dd05

---
+++ d05/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+	public GameObject	ball;
+	public GameObject	flag1;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.placeAtBall(flag1.transform);
+	}
+
+	private void placeAtBall(Transform target)
+	{
+		transform.LookAt(target);
+		transform.position = new Vector3(ball.transform.position.x - 3, ball.transform.position.y + 1, ball.transform.position.z);
+	}
+}
---
+++ d05/Assets/Scripts/gameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour {
+
+	public GameObject	ball;
+	public GameObject	flag1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space"))
+		{
+			Vector3 tmp = flag1.transform.position - ball.transform.position;
+			tmp = tmp.normalized;
+			tmp += new Vector3(0, 1, 0);
+			ball.GetComponent<Rigidbody>().AddForce(tmp * 30, ForceMode.Impulse);
+		}
+	}
+}
