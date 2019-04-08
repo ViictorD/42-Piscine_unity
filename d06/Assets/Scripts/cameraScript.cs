5936b8abb1f7b6fec67dfe87d161e7f7cfe34413
Your Name <you@example.com>
2019-04-08 19:25:58 +0200

    a

---
+++ d06/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+	public float camSens = 0.25f;
+	public GameObject	player;
+	private Camera		mycam;
+	private Vector3 lastMouse = new Vector3(255, 255, 255);
+
+	// Use this for initialization
+	void Start () {
+		this.mycam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lastMouse = Input.mousePosition - lastMouse;
+		lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
+		lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
+		transform.eulerAngles = lastMouse;
+		lastMouse = Input.mousePosition;
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z);
+	}
+
+}
---
+++ d06/Assets/Scripts/playerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+
+	public float mainSpeed = 0.8f;
+	private CharacterController	cc;
+
+	// Use this for initialization
+	void Start () {
+		this.cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Vector3	v = new Vector3(0, 0, Input.GetAxis("Vertical"));
+		// v = transform.TransformDirection(v);
+		// v *= this.mainSpeed;
+		// transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * 10);
+		// this.cc.Move(v);
+		Vector3	v = new Vector3();
+		if (Input.GetKey(KeyCode.W))
+			v += Camera.main.transform.forward.normalized;
+		if (Input.GetKey(KeyCode.S))
+			v += Camera.main.transform.forward.normalized * -1;
+		if (Input.GetKey(KeyCode.A))
+			v += Camera.main.transform.right.normalized * -1;
+		if (Input.GetKey(KeyCode.D))
+			v += Camera.main.transform.right.normalized;
+
+		v = v.normalized * mainSpeed;
+		Debug.Log(v);
+		this.cc.Move(v.normalized);
+		// .transform.Translate(v * Time.deltaTime);
+	}
+}
