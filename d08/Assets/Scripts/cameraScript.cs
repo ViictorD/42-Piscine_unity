f509f7fa80f937945dec30ef28e0e55521f9bb8a
Your Name <you@example.com>
2019-04-12 22:23:16 +0200

    start d08

---
+++ d08/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		//this.player = GameObject.FindGameObjectWithTag("Player");
+		this.offset = player.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = this.player.transform.position - this.offset;
+		transform.LookAt(this.player.transform);
+	}
+}
---
+++ d08/Assets/Scripts/mayaScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class mayaScript : MonoBehaviour {
+
+	enum characterState { IDLE, RUN, ATTACK, DIE };
+
+	private Animator animator;
+	private NavMeshAgent navMesh;
+	private characterState state;
+
+
+	// Use this for initialization
+	void Start () {
+		this.animator = GetComponent<Animator>();
+		this.navMesh = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+		{
+			Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit ray;
+			if (Physics.Raycast(r, out ray))
+			{
+				// Mettre une rotation du perso vers la ou on a clique
+				this.navMesh.destination = ray.point;
+				this.animator.SetTrigger("mayaRun");
+				this.state = characterState.RUN;
+			}
+			
+
+		}
+		if (this.state == characterState.RUN && this.navMesh.destination == transform.position)
+		{
+			this.animator.SetTrigger("mayaIdle");
+			this.state = characterState.IDLE;
+		}
+	}
+}
